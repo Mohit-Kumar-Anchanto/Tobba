5893e5b3490874034214b3d4271bd05b19a8c690
Mohit-Kumar-Anchanto <mohit.kumar@anchanto.com>
2021-11-29 19:04:01 +0530

    Working Services

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,30 +1,27 @@
 class ProductController < ApplicationController
 
-  def initialize
-    @bunny_connection = Bunny.new
-  end
-
-  def create_product(payload)
+  def self.create_product(payload)
     payload = JSON.parse(payload)
     product_name = payload["name"]
     stock = payload["stock"]
     Product.find_or_create_by(name: product_name, stock: stock)
   end
 
-  def get_payload
-    consumer = ConsumerServices.new
+  def self.get_payload
+    consumer = ConsumerService.new
     queue = consumer.declare_queue("CreateProduct")
     payload = consumer.consume_message(queue)
-    create_product(payload) if payload.present?
+    puts "Payload Received #{payload}"
+    ProductController.create_product(payload) if payload.present?
     return false
   end
 
-  def send_request_to_create_order(order_number, price)
-    publisher = PublisherServices.new
+  def self.send_request_to_create_order(order_number, price)
+    publisher = PublisherService.new
     queue = publisher.declare_queue("CreateOrder")
     payload = { number: order_number, price: price}
     payload = payload.to_json
-    publisher.publish_message(payload, queue.name)
+    publisher.publish_message(queue.name, payload)
     publisher.close_connection
   end
 
